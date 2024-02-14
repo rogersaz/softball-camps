fc3433fac1101a9cdc9086a1e796f3c9efe274bb
Mark <45799393+rogersaz@users.noreply.github.com>
2024-02-13 20:44:39 -0700

    commit out lined logos at the bottom

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -63,7 +63,7 @@ export default function Index() {
 
           </div>
         </div>
-<!--
+{/*
         <div className="mx-auto max-w-7xl py-2 px-4 sm:px-6 lg:px-8">
           <div className="mt-6 flex flex-wrap justify-center gap-8">
             {[
@@ -115,9 +115,7 @@ export default function Index() {
               >
                 <img alt={img.alt} src={img.src} />
               </a>
-              
--->
-              
+*/}              
               
             ))}
           </div>
