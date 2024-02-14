524c385c4e43474872e2d74037d79bccdea243cc
Mark <45799393+rogersaz@users.noreply.github.com>
2024-02-13 20:36:56 -0700

    commit out logos at the bottom

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -19,7 +19,7 @@ export default function Index() {
             <div className="lg:pb-18 relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-8 lg:pt-32">
               <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
                 <span className="block uppercase text-violet-500 drop-shadow-md">
-                  A1 OF 5'1" 2nd Year L/L
+                  A1<br>Outfield- 2nd Year - L/L
                 </span>
               </h1>
               <p className="mx-auto mt-6 max-w-lg text-center text-xl text-white sm:max-w-3xl">
@@ -51,6 +51,7 @@ export default function Index() {
                   </div>
                 )}
               </div>
+                
               <a href="https://remix.run">
                 <img
                   src="https://user-images.githubusercontent.com/1500684/158298926-e45dafff-3544-4b69-96d6-d3bcc33fc76a.svg"
@@ -59,9 +60,10 @@ export default function Index() {
                 />
               </a>
             </div>
+
           </div>
         </div>
-
+<!--
         <div className="mx-auto max-w-7xl py-2 px-4 sm:px-6 lg:px-8">
           <div className="mt-6 flex flex-wrap justify-center gap-8">
             {[
@@ -113,6 +115,10 @@ export default function Index() {
               >
                 <img alt={img.alt} src={img.src} />
               </a>
+              
+-->
+              
+              
             ))}
           </div>
         </div>
