bec6d140614b857372c1594c160930f21cf958b0
Mark <45799393+rogersaz@users.noreply.github.com>
2024-02-15 15:46:04 -0700

    Update index.tsx text size

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -17,14 +17,14 @@ export default function Index() {
               <div className="absolute inset-0 bg-[color:rgba(139,92,246,0.5)] mix-blend-multiply" />
             </div>
             <div className="lg:pb-18 relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-8 lg:pt-32">
-              <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
+              <h1 className="text-center text-4xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
                 <span className="block uppercase text-violet-500 drop-shadow-md">
-                  - a1 -
+                  a1
                 </span>
-                 <span className="block uppercase text-violet-250 drop-shadow-md">
+                 <span className="block uppercase text-violet-500 drop-shadow-md">
                    LOUISIANA TECH
                 </span>
-                 <span className="block uppercase text-violet-250 drop-shadow-md">
+                 <span className="block uppercase text-violet-500 drop-shadow-md">
                    OF - L/L
                 </span>
               </h1>
