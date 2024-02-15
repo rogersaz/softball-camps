a35b5273cfa8fa60f5438d554286c2f5ac735b02
Mark <45799393+rogersaz@users.noreply.github.com>
2024-02-14 22:07:03 -0700

    Update index.tsx

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -19,13 +19,13 @@ export default function Index() {
             <div className="lg:pb-18 relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-8 lg:pt-32">
               <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
                 <span className="block uppercase text-violet-500 drop-shadow-md">
-                  alannah a1 rogers
+                  - a1 -
                 </span>
                  <span className="block uppercase text-violet-500 drop-shadow-md">
                    LOUISIANA TECH
                 </span>
                  <span className="block uppercase text-violet-500 drop-shadow-md">
-                   Outfield- 2nd Year - L/L
+                   OF - L/L
                 </span>
               </h1>
         {/*
